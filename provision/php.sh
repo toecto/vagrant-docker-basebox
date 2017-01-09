0f460e70b829ef35a0a05c75f47ff01a331f7f32
Maxim <ad@zalil.ru>
2017-01-09 12:11:21 -0500

    Update php.sh

--- provision/php.sh
+++ provision/php.sh
@@ -4,12 +4,12 @@ add-apt-repository -y ppa:ondrej/php
 
 apt-get -y update
 apt-get install -y --no-install-recommends \
-    php5.5-cli \
-    php5.5-curl \
-    php5.5-mcrypt \
-    php5.5-mbstring \
-    php5.5-json \
-    php5.5-curl
+    php5.6-cli \
+    php5.6-curl \
+    php5.6-mcrypt \
+    php5.6-mbstring \
+    php5.6-json \
+    php5.6-curl
 
 
 echo "php is installed"
